5ff99d8cd5466e6f7aae18cf5543502f670aaffd
Yaron <yaron.mic@gmail.com>
2014-06-20 14:36:57 +0300

    Added Polling. And a few other changed.

--- app.js
+++ app.js
@@ -7,17 +7,35 @@ app.use(compression());
 app.use(bodyParser());
 app.use(express.static(__dirname + '/root', { maxAge: 86400000 /*one day*/ }));
 
+var memos = [];
+var lastIndex = 0;
 
-var memos = [
-	{id : 1,title : 'Remember', text : 'The Milk'},
-	{id : 2,title : 'Do not remember', text : 'The Milk'}
-];
-var lastIndex = 2;
-app.get('/memos', function(req, res) {
-	res.json(memos);
+app.post('/memos/update', function(req, res) {
+	if(	req.body.ids instanceof Array) {
+		var ret = {};
+		ret.removed = req.body.ids.filter(function (id) {
+			return memos.every(function (memo) {
+				return memo.id != id;
+			});
+		});
+		console.log('User had these Ids : ' + req.body.ids)
+		console.log('Server had : ' + JSON.stringify(memos))
+		ret.added = memos.filter(function (memo) {
+			return req.body.ids.indexOf(memo.id+'') <= -1;
+		})
+		console.log('User will recieve these : ' + ret.added)
+		console.log('')
+		console.log('')
+		res.json(ret);
+	} else {
+		return res.json({added : memos});
+	}
 });
 app.post('/memo/', function(req, res) {
-	if(req.body.title && req.body.text) {
+	if(	(typeof(req.body.title) == "string" || req.body.title instanceof String) && 
+		(typeof(req.body.text) == "string"  || req.body.text instanceof String)&& 
+		req.body.title.trim().length != 0 &&
+		req.body.text.trim().length != 0) {
 		memos.push({ 
 			title : req.body.title, 
 			text : req.body.text, 
@@ -33,4 +51,5 @@ app.delete('/memo/:id', function(req, res) {
 	memos = memos.filter(function(item){return item.id != req.params.id});
 	res.json(true);
 });
-app.listen(process.env.PORT || 3000);
+var port = process.env.PORT || 5000;
+app.listen(port);
